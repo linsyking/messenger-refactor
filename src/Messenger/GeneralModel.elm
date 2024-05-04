61ee97f0807426c45da776d0e4569f82f700d305
linsyking <kxiang@umich.edu>
2024-05-04 10:59:29 -0400

    feat: add core

---
+++ src/Messenger/GeneralModel.elm
@@ -0,0 +1,53 @@
+module Messenger.GeneralModel exposing
+    ( GeneralModel
+    , viewModelList, viewModelArray
+    )
+
+{-|
+
+
+# General Model
+
+General model is designed to be an abstract interface of scenes, layers, components, game components, etc..
+
+  - a: data type
+  - b: environment type
+  - c: message type
+  - d: target type
+  - e: render type
+
+@docs GeneralModel
+@docs viewModelList, viewModelArray
+
+-}
+
+import Array exposing (Array)
+
+
+{-| General Model.
+
+This has a name field.
+
+-}
+type alias GeneralModel a b c d e =
+    { name : String
+    , data : a
+    , update : b -> a -> ( a, List ( d, c ), b )
+    , updaterec : b -> c -> a -> ( a, List ( d, c ), b )
+    , view : b -> a -> e
+    }
+
+
+{-| View model list.
+-}
+viewModelList : b -> List (GeneralModel a b c d e) -> List e
+viewModelList env models =
+    List.map (\model -> model.view env model.data) models
+
+
+{-| View model array.
+-}
+viewModelArray : b -> Array (GeneralModel a b c d e) -> List e
+viewModelArray env models =
+    Array.toList models
+        |> List.map (\model -> model.view env model.data)
---
+++ src/Messenger/Recursion.elm
@@ -0,0 +1,71 @@
+module Messenger.Recursion exposing
+    ( Updater, Matcher, Super, Cleaner, Patcher
+    , RecBody
+    )
+
+{-|
+
+
+# Recursion
+
+This module provides the signature for the updater
+
+@docs Updater, Matcher, Super, Cleaner, Patcher
+@docs RecBody
+
+-}
+
+
+{-| The updater
+a: message sender (object)
+b: message
+c: environment messages
+d: target
+-}
+type alias Updater a b c d =
+    a -> c -> ( a, List ( d, b ), c )
+
+
+{-| The recursive updater
+-}
+type alias RecUpdater a b c d =
+    a -> c -> b -> ( a, List ( d, b ), c )
+
+
+{-| Return true if the target is the sender (second argument)
+-}
+type alias Matcher a d =
+    a -> d -> Bool
+
+
+{-| Return true if the target is the parent
+-}
+type alias Super d =
+    d -> Bool
+
+
+{-| Clean the environment
+-}
+type alias Cleaner c =
+    c -> c
+
+
+{-| Patch the environment
+-}
+type alias Patcher c =
+    c -> c -> c
+
+
+{-| RecBody type.
+
+Pass this as an argument to the updater
+
+-}
+type alias RecBody a b c d =
+    { update : Updater a b c d
+    , updaterec : RecUpdater a b c d
+    , match : Matcher a d
+    , super : Super d
+    , clean : Cleaner c
+    , patch : Patcher c
+    }
