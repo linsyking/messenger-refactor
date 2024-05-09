220369f9296bed04c825d085ad03e4ab3c8d8028
linsyking <kxiang@umich.edu>
2024-05-09 19:58:03 -0400

    feat: port to main

--- src/Messenger/UI.elm
+++ src/Messenger/UI.elm
@@ -2,12 +2,16 @@ module Messenger.UI exposing (..)
 
 import Audio
 import Messenger.Base exposing (Flags, WorldEvent)
-import Messenger.Model exposing (Model)
+import Messenger.Model exposing (Model, audio)
 import Messenger.Scene.Loader exposing (SceneStorage)
+import Messenger.UI.Init exposing (init)
+import Messenger.UI.Subscription exposing (subscriptions)
+import Messenger.UI.Update exposing (update)
+import Messenger.UI.View exposing (view)
 import Messenger.UserConfig exposing (UserConfig)
 
 
-type alias Scenes localstorage scenemsg=
+type alias Scenes localstorage scenemsg =
     List ( String, SceneStorage localstorage scenemsg )
 
 
@@ -17,5 +21,17 @@ type alias Input localstorage scenemsg =
     }
 
 
-type alias Output localstorage scenemsg=
+type alias Output localstorage scenemsg =
     Program Flags (Audio.Model WorldEvent (Model localstorage scenemsg)) (Audio.Msg WorldEvent)
+
+
+main : Input localstorage scenemsg -> Output localstorage scenemsg
+main input =
+    Audio.elementWithAudio
+        { init = init input.config input.allScenes
+        , update = update input.config input.allScenes
+        , subscriptions = subscriptions input.config
+        , view = view input.config
+        , audio = audio
+        , audioPort = { toJS = audioPortToJS, fromJS = audioPortFromJS }
+        }
